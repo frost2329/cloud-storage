cfc55eb4710c6f0c94e606d28dc21cdeb3df1251
frost2329 <morozzzov.da@gmail.com>
2025-12-24 12:22:45 +0300

    refactoring: local docker-compose

--- src/main/java/com/frostetsky/cloudstorage/constants/MinioConstants.java
+++ src/main/java/com/frostetsky/cloudstorage/constants/MinioConstants.java
@@ -9,6 +9,4 @@ public class MinioConstants {
     public static final int PART_SIZE = -1;
     public static final ByteArrayInputStream EMPTY_DIR_BYTEARRAY_STREAM = new ByteArrayInputStream(new byte[0]);
     public static final long EMPTY_DIR_SIZE = 0L;
-
-
 }
